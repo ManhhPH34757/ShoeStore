c5e86ea96a46655b508ce96abef4b684d4bf5929
ManhhPH34757 <nguyenmanh2707ngt@gmail.com>
2024-06-28 15:47:37 +0700

    Create project

---
+++ src/test/java/com/spring/shoestore/ShoeStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.shoestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoeStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
